e6743ddff21653f0c0ceec2a40bb97f2459ab217
Christian Schneider <fuzzyherbivore@icloud.com>
2025-01-07 20:35:50 +0100

    Update level 01 to new naming for spawn point.

--- levels/level.gd
+++ levels/level.gd
@@ -5,6 +5,6 @@ func _ready() -> void:
 	var player_scene = preload("res://player/player.tscn")
 	
 	var player = player_scene.instantiate()
-	player.transform = %PlayerSpawnPoint.transform
+	player.transform = $PlayerSpawnPoint.transform
 	
 	get_parent().add_child.call_deferred(player)
