317b40119a231507be0fe59c6289015988236387
TomGrzembke <grzembketom@gmail.com>
2025-02-19 15:11:18 +0000

    WIP idle randomization through script

--- player/player_animatables.gd
+++ player/player_animatables.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export_range(0, 100, 1) var idle2_probability : float
+@export var idle_animations : Dictionary = {"idling" : 70, "idling2": 20, "idling3": 10}
 @onready var player: CharacterBody2D = $".."
 @onready var abilities: Node2D = $"../Abilities"
 @onready var animation_tree: AnimationTree = $AnimationTree
@@ -8,19 +10,28 @@ var state_machine
 
 func _ready():
 	state_machine = animation_tree.get("parameters/playback")
-	abilities.player_hits.connect(func(): state_machine.travel("hit"))
-	abilities.used_ability.connect(func(): state_machine.travel("dash"))
+	abilities.player_hits.connect(func(): state_machine.start("hit"))
+	abilities.used_ability.connect(func(): state_machine.start("dash"))
 
-	player.player_despawned.connect(func(): state_machine.travel("dying"))
-	player.player_gets_pushed.connect(func(): state_machine.travel("got_hit"))
-
-
-func hit():
-	state_machine.travel("hit")
+	player.player_despawned.connect(func(): state_machine.start("dying"))
+	player.player_gets_pushed.connect(func(): state_machine.start("got_hit"))
 
 
 func _on_animation_finished(anim_name):
-	if anim_name == "idling":
-		state_machine.travel("idling2")
-	elif anim_name == "idling2":
-		state_machine.travel("idling")
+	different_idles(anim_name)
+
+
+func different_idles(anim_name):
+	if !anim_name in idle_animations: return
+
+	var rng_percent = randi_range(0, 100)
+	print(rng_percent)
+
+	for key in idle_animations.keys():
+		var value = idle_animations[key]
+		if value > rng_percent:
+			continue
+
+		state_machine.start(key, true)
+		print(key)
+		return
