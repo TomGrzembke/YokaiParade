8031b16acd17069cd36d9ee8cdc9b1435529fcf0
Christian Schneider <christian.schneider@school4games.net>
2025-03-12 17:05:38 +0100

    Merged

--- sounds/sound_player.gd
+++ sounds/sound_player.gd
@@ -2,6 +2,7 @@ extends AudioStreamPlayer2D
 @export_range(0.0, 2.0) var pitch_min : float = 1.0
 @export_range(0.0, 2.0) var pitch_max : float = 1.0
 
+
 func emit_sound(sound : AudioStream):
 	set_stream(sound)
 	play()
