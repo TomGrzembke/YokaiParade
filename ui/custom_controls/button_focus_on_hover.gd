7f9128e512f1b6a2768d6f715d1cb7a93b0d37ea
TomGrzembke <grzembketom@gmail.com>
2025-03-12 17:04:06 +0000

    Simplified sound emitting

--- ui/custom_controls/button_focus_on_hover.gd
+++ ui/custom_controls/button_focus_on_hover.gd
@@ -1,24 +1,27 @@
 extends Button
 
-@onready var ui_audio_stream_player_hover = %UiAudioStreamPlayerHover
-@onready var ui_audio_stream_player_click = %UiAudioStreamPlayerClick
-
+var hover_player
+var click_player
 
 func _ready():
 	mouse_entered.connect(on_hover)
 	focus_entered.connect(on_hover)
+	hover_player = get_node_in_game("Game/UiAudioStreamPlayerHover")
+	click_player = get_node_in_game("Game/UiAudioStreamPlayerClick")
 
 
 func _pressed():
 	disabled = true
-	if ui_audio_stream_player_click != null:
-		ui_audio_stream_player_click.play()
+	click_player.play()
 
 
 func on_hover():
 	grab_focus()
-	if ui_audio_stream_player_hover != null:
-		ui_audio_stream_player_hover.play()
+	hover_player.play()
+
+
+func get_node_in_game(node_patch):
+	return get_tree().root.get_node(node_patch)
 
 
 func _exit_tree():
